8750cce75c2cb1d358657c97703192a332fb9881
Srijan <srijankarki0@gmail.com>
2023-11-29 15:48:15 +0545

    showing details from App.js in a site

--- react/sri/src/FirstComponent.js
+++ react/sri/src/FirstComponent.js
@@ -4,6 +4,18 @@ const FirstComponent = (abc) => {
       <p>Creating First Component</p>
       <h1>{abc.name}</h1>
       <h2>{abc.courseName}</h2>
+      <p>
+        {abc.details.name} is {abc.age} years old and has{" "}
+        {abc.details.bloodGroup} and lives in {abc.details.address} is{" "}
+        {abc.isStudent ? " " : "not"} a student, his subjects are:{" "}
+        {/*abc.subjects.join(", ")*/}
+      </p>
+      <ul>
+        {/*mapping array to use it*/}
+        {abc.subjects.map((s) => (
+          <li key={s}>{s}</li>
+        ))}
+      </ul>
     </div>
   );
 };
